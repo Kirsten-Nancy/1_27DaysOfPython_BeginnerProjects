943d8ed0dfd1897d659f272aad56522997c52e08
Kirsten-Nancy <korjnan@gmail.com>
2021-01-12 12:24:12 +0800

    added an edge case to change turtle pos

--- Hirst_Painting_Day18/main.py
+++ Hirst_Painting_Day18/main.py
@@ -10,15 +10,19 @@ color_data = [(43, 105, 171), (233, 206, 116), (225, 152, 87), (183, 50, 77), (1
               (214, 61, 80), (109, 110, 188), (130, 175, 210), (115, 185, 139), (55, 176, 110), (116, 168, 37),
               (202, 18, 42), (33, 56, 113), (221, 61, 50), (26, 142, 108)]
 
+# Moving the turtle to the bottom
 my_turtle.hideturtle()
 my_turtle.penup()
 my_turtle.goto(-250.0, -250.0)
 my_turtle.showturtle()
 
 def hirst_painting():
-    for _ in range(10):
-        my_turtle.fd(50)
+    for i in range(10):
         my_turtle.dot(20, random.choice(color_data))
+        # To prevent it from moving forward at the last iteration
+        if i == 9:
+            break
+        my_turtle.fd(50)
 
     my_turtle.left(90)
     my_turtle.fd(50)
